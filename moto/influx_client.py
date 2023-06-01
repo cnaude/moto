95143cc96eb55b8e294a5185414afd58a85bb931
Chris Naude <chris.naude.0@gmail.com>
2023-06-01 10:11:16 -0700

    ssl verify option

--- moto/influx_client.py
+++ moto/influx_client.py
@@ -38,16 +38,19 @@ class InfluxClient:  # pylint: disable=too-few-public-methods
         token: str | None = None,
         org: str | None = None,
         bucket: str | None = None,
+        ssl_verify: bool | None = False,
     ):
         self._url = url or os.environ.get("INFLUXDB_URL", "http://localhost:8086")
         self._token = token or os.environ["INFLUXDB_TOKEN"]
         self._org = org or os.environ["INFLUXDB_ORG"]
         self._bucket = bucket or os.environ["INFLUXDB_BUCKET"]
+        self._ssl_verify = bucket or os.environ["INFLUXDB_SSL_VERIFY"]
 
         self._client = InfluxDBClient(
             url=self._url,
             token=self._token,
             org=self._org,
+            ssl_verify=self._ssl_verify,
         )
 
         self._write_api = self._client.write_api(write_options=SYNCHRONOUS)
